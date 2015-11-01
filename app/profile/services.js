d8e935fb5f0a09a95dddd6f2d7b2bdd80b3ebbef
Velmurugan Velayutham <mailtovelmuruga@gmail.com>
2015-11-01 18:53:43 +0000

    added json api server for fast protyping

--- app/profile/services.js
+++ app/profile/services.js
@@ -1,14 +1,27 @@
 'use strict';
-angular.module('myApp.profile.services',[]).factory('profileService',function(){
+angular.module('myApp.profile.services',[]).factory('profileService',['$http',function($http){
+
 	return{          
-       users:[{name:"velmurugan"},{name:"manick"}],
-       listUsers:function(){
-       	this.users;
+     listUsers:function(){
+      var promise = $http({
+			  method: 'GET',
+			  url: 'http://localhost:3000/people'
+			}).then(function successCallback(response) {
+				console.log(JSON.stringify(response.data));
+			     return response.data;
+			  }, function errorCallback(response) {
+			    alert('error');
+			  });
+			return promise;
        }
 	};
-}).
+}]).
 controller('ProfileController',['$scope','profileService', function($scope,profileService){
 
-		$scope.users=[{name:"velmurugan"},{name:"manick"}];
+		profileService.listUsers().then(function(data){
+			$scope.users=data;
+			console.log($scope.users);
+		})
+		
 
 }]);
