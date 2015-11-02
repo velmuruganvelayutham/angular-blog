0c220a228668f0ac92742498de006a530849d822
Velmurugan Velayutham <mailtovelmuruga@gmail.com>
2015-11-02 03:21:43 +0000

    added form to add new user

--- app/profile/services.js
+++ app/profile/services.js
@@ -10,18 +10,42 @@ angular.module('myApp.profile.services',[]).factory('profileService',['$http',fu
 				console.log(JSON.stringify(response.data));
 			     return response.data;
 			  }, function errorCallback(response) {
-			    alert('error');
+			     alert('error');
+			  });
+			return promise;
+       },
+
+       addUser:function(user){
+         var promise = $http({
+			  method: 'POST',
+			  url: 'http://localhost:3000/people',
+			  data:user
+			}).then(function successCallback(response) {
+				 console.log(JSON.stringify(response.data));
+			     return response.data;
+			  }, function errorCallback(response) {
+			     alert('error');
 			  });
 			return promise;
        }
-	};
+
+   };
 }]).
 controller('ProfileController',['$scope','profileService', function($scope,profileService){
 
-		profileService.listUsers().then(function(data){
+		$scope.listUsers = function(){
+			profileService.listUsers().then(function(data){
 			$scope.users=data;
 			console.log($scope.users);
 		})
+	};
+		$scope.addUser = function(user){
+			console.log(user);
+			profileService.addUser(user).then(function(data){
+			
+		})
+		};
 		
+		$scope.listUsers();
 
 }]);
