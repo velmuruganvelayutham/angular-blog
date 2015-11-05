8436e0436c906fc3bbb03ca938411e07e8a1c9b3
velmurugan <mailtovelmuruga@gmail.com>
2015-11-05 10:30:11 -0500

    added directives

--- app/app.js
+++ app/app.js
@@ -2,25 +2,66 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.profile',
-  'myApp.profile.services',
-  'myApp.version'
+    'ngRoute',
+    'myApp.profile.services',
+    'myApp.directive.panel',
+    'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/dashboard',{templateUrl:'dashboard.html'})
-  .otherwise({redirectTo: '/dashboard'});
-}]).controller('todoController',['$scope',function($scope){
+config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-    $scope.todoList=[];
-    $scope.addToDo=function(value){
-      $scope.todoList.push({text:value});
-      $scope.todo="";
+    $routeProvider.when('/dashboard', {
+        templateUrl: 'dashboard.html'
+    });
+    $routeProvider.when('/contact', {
+        templateUrl: 'dashboard.html'
+    });
+    $routeProvider.when('/login', {
+        templateUrl: 'profile/login.html',
+        controller: 'ProfileController'
+    });
+    $routeProvider.when('/signup', {
+        templateUrl: 'profile/signup.html'
+    });
+    $routeProvider.when('/users', {
+        templateUrl: 'profile/users.html',
+        controller: 'ProfileController'
+    }).otherwise({
+        redirectTo: '/dashboard'
+    });
+
+}]).run(function($rootScope, $location) {
+    $rootScope.$on("$routeChangeStart", function(event, next, current) {
+        if ($rootScope.loggedInUser == null) {
+            // no logged user, redirect to /login
+            if (next.templateUrl === "profile/login.html") {} else {
+                $location.path("/login");
+            }
+        }
+    });
+}).controller('TodoController', ['$scope', function($scope) {
+
+    $scope.todoList = [];
+    $scope.addToDo = function(value) {
+        $scope.todoList.push({
+            text: value
+        });
+        $scope.todo = "";
     }
-    $scope.removeToDo=function(value){
-      if($scope.todoList.indexOf(value)!==-1){
-        $scope.todoList.splice($scope.todoList.indexOf(value),1);
-      }
+    $scope.removeToDo = function(value) {
+        if ($scope.todoList.indexOf(value) !== -1) {
+            $scope.todoList.splice($scope.todoList.indexOf(value), 1);
+        }
+    }
+
+}]).controller('MainController', ['$scope', '$location', function($scope, $location) {
+
+    $scope.menuClass = function(page) {
+        var current = $location.path().substring(1);
+        if (current === page) {
+            return "active";
+        } else {
+            return "";
+        }
     }
 
 }]);
--- app/profile/services.js
+++ app/profile/services.js
@@ -1,127 +1,132 @@
 'use strict';
-angular.module('myApp.profile.services',[]).factory('profileService',['$http',function($http){
+angular.module('myApp.profile.services', []).factory('profileService', ['$http', function($http) {
 
-	return{          
-     listUsers:function(){
-      var promise = $http({
-			  method: 'GET',
-			  url: 'http://localhost:3000/people'
-			}).then(function successCallback(response) {
-				console.log(JSON.stringify(response.data));
-			     return response.data;
-			  }, function errorCallback(response) {
-			     alert('error');
-			  });
-			return promise;
-       },
+    return {
+        listUsers: function() {
+            var promise = $http({
+                method: 'GET',
+                url: 'http://localhost:3000/people'
+            }).then(function successCallback(response) {
+                console.log(JSON.stringify(response.data));
+                return response.data;
+            }, function errorCallback(response) {
+                alert('error');
+            });
+            return promise;
+        },
 
-       addUser:function(user){
-         var addPromise = $http({
-			  method: 'POST',
-			  url: 'http://localhost:3000/people',
-			  data:user
-			}).then(function successCallback(response) {
-				 console.log(JSON.stringify(response.data));
-			     return response.data;
-			  }, function errorCallback(response) {
-			     alert('error');
-			  });
-			return addPromise;
-       }
-       ,
+        addUser: function(user) {
+            var addPromise = $http({
+                method: 'POST',
+                url: 'http://localhost:3000/people',
+                data: user
+            }).then(function successCallback(response) {
+                console.log(JSON.stringify(response.data));
+                return response.data;
+            }, function errorCallback(response) {
+                alert('error');
+            });
+            return addPromise;
+        },
 
-       deleteUser:function(id){
-         var deletePromise = $http({
-			  method: 'DELETE',
-			  url: 'http://localhost:3000/people/'+ id
-			}).then(function successCallback(response) {
-				 console.log(JSON.stringify(response.data));
-			     return response.data;
-			  }, function errorCallback(response) {
-			     alert('error');
-			  });
-			return deletePromise;
-       },
-        updateUser:function(user){
-         var updatePromise = $http({
-			  method: 'PUT',
-			  url: 'http://localhost:3000/people/'+ user.id,
-			  data:user
-			}).then(function successCallback(response) {
-				 console.log(JSON.stringify(response.data));
-			     return response.data;
-			  }, function errorCallback(response) {
-			     alert('error');
-			  });
-			return updatePromise;
-       }
+        deleteUser: function(id) {
+            var deletePromise = $http({
+                method: 'DELETE',
+                url: 'http://localhost:3000/people/' + id
+            }).then(function successCallback(response) {
+                console.log(JSON.stringify(response.data));
+                return response.data;
+            }, function errorCallback(response) {
+                alert('error');
+            });
+            return deletePromise;
+        },
+        updateUser: function(user) {
+            var updatePromise = $http({
+                method: 'PUT',
+                url: 'http://localhost:3000/people/' + user.id,
+                data: user
+            }).then(function successCallback(response) {
+                console.log(JSON.stringify(response.data));
+                return response.data;
+            }, function errorCallback(response) {
+                alert('error');
+            });
+            return updatePromise;
+        }
 
-   };
+    };
 }]).
-controller('ProfileController',['$scope','profileService','$filter',function($scope,profileService,$filter){
-		$scope.user={};
-		$scope.listUsers = function(){
-			profileService.listUsers().then(function(data){
-			$scope.users=data;
-			console.log($scope.users);
-		})
-	};
-		$scope.addUser = function(user){
-			console.log(user);
-            var upperCaseFilter=$filter('uppercase');
-            user.name=upperCaseFilter(user.name);
-			if($scope.update){
+controller('ProfileController', ['$scope', '$location', '$rootScope', 'profileService', '$filter', function($scope, $location, $rootScope, profileService, $filter) {
+    $scope.user = {};
+    $scope.listUsers = function() {
+        profileService.listUsers().then(function(data) {
+            $scope.users = data;
+            console.log($scope.users);
+        })
+    };
+    $scope.addUser = function(user) {
+        console.log(user);
+        var upperCaseFilter = $filter('uppercase');
+        user.name = upperCaseFilter(user.name);
+        if ($scope.update) {
 
-			profileService.updateUser(user).then(function(data){
-				$scope.resetUser();
-				$scope.listUsers();
-				$scope.update=false;
-				$scope.message=user.name+ " is updated successfully !";
-		})
-			}else{
-				
-				profileService.addUser(user).then(function(data){
-				$scope.resetUser();
-				$scope.listUsers();
-				$scope.message=user.name+ " is added successfully !";
-		})
-			}
-			
-		};
-		$scope.editUser= function(user){
-				$scope.message="";
-				$scope.user=user;
-				$scope.update=true;
-		}
+            profileService.updateUser(user).then(function(data) {
+                $scope.resetUser();
+                $scope.listUsers();
+                $scope.update = false;
+                $scope.message = user.name + " is updated successfully !";
+            })
+        } else {
 
-		$scope.deleteUser = function(id){
-			console.log(id);
-			profileService.deleteUser(id).then(function(data){
-				$scope.message= id + " is deleted successfully !";
-				$scope.listUsers();
-              
-		})
-		};
-		$scope.resetUser =function(){
-			$scope.user={};
-			$scope.message="";
-		}
-		
-		$scope.listUsers();
+            profileService.addUser(user).then(function(data) {
+                $scope.resetUser();
+                $scope.listUsers();
+                $scope.message = user.name + " is added successfully !";
+            })
+        }
 
-		$scope.$watch('user.id',function(newVal,oldVal){
-			console.log(newVal+ "- -"+ oldVal);
-			if(!newVal)return ;
-			if(newVal.length<3){
-				$scope.message='minimum 3 characters are required!.';
-			}else{
-				$scope.message='';
-			}
-		});
+    };
+    $scope.editUser = function(user) {
+        $scope.message = "";
+        $scope.user = user;
+        $scope.update = true;
+    }
 
-}]).filter('userIdGenerator',function(){
+    $scope.deleteUser = function(id) {
+        console.log(id);
+        profileService.deleteUser(id).then(function(data) {
+            $scope.message = id + " is deleted successfully !";
+            $scope.listUsers();
 
-	return function(input){
-		return input.name + '@angular-blog.io';
-	}
+        })
+    };
+    $scope.resetUser = function() {
+        $scope.user = {};
+        $scope.message = "";
+    }
+
+    $scope.login = function() {
+
+        $rootScope.loggedInUser = true;
+        $location.path('/dashboard');
+
+    }
+    $scope.listUsers();
+
+    $scope.$watch('user.id', function(newVal, oldVal) {
+        console.log(newVal + "- -" + oldVal);
+        if (!newVal) return;
+        if (newVal.length < 3) {
+            $scope.message = 'minimum 3 characters are required!.';
+        } else {
+            $scope.message = '';
+        }
+    });
+
+}]).filter('userIdGenerator', function() {
+
+    return function(input) {
+        return input.name + '@angular-blog.io';
+    }
 });
